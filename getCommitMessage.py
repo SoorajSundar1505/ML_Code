1e734191cfe4a3cd87e4810ca2c50c206f179234
Suraj S <130204034+SoorajSundar1505@users.noreply.github.com>
2023-12-28 14:02:30 +0530

    Update getCommitMessage.py

--- getCommitMessage.py
+++ getCommitMessage.py
@@ -18,32 +18,38 @@ def predict_commit_outcome(commit_message, model_path='model.pkl', vectorizer_pa
     # return str(outcome_prediction[0])
     # Redirect the standard output to a file
 
-  if outcome_prediction[0]==1:
-    keywords = ['login', 'home']  # Replace with your actual keywords
+  if outcome_prediction[0] == 1:
+        # Define your keywords
+        keywords = ['keyword1', 'keyword2', 'keyword3']  # Replace with your actual keywords
 
         # Check if any keyword is present in the commit message
         matched_keywords = [kw for kw in keywords if kw in commit_message.lower()]
 
-        # If any keyword is matched, save the matched keyword to a file
+        # If any keyword is matched, save the commit message and matched keyword(s) to a file
         if matched_keywords:
             with open("output.txt", "w") as f:
+                print(f"Commit Message: {commit_message}", file=f)
+                print("Matched Keywords:", file=f)
                 for matched_keyword in matched_keywords:
                     print(matched_keyword, file=f)
+            return matched_keywords
         else:
             print("No matching keywords found in the commit message.")
+            return None  # or return some default value
     else:
         print("Prediction outcome is not 1. Skipping keyword check.")
-    
-    with open("output.txt", "w") as f:
-        print(outcome_prediction[0], file=f)
-
-    # Exit with a success code
-    sys.exit(0)
-
+        return None  # or return some default value
 
 if __name__ == "__main__":
     import sys
+
     commit_message = sys.argv[1] if len(sys.argv) > 1 else "No Commit Message Found"
-    print(f"Received commit message: {sys.argv[1]}")
-    predicted_outcome = predict_commit_outcome(commit_message)
-    print(f"Received Outcome is: {predicted_outcome}")
+    print(f"Received commit message: {commit_message}")
+
+    # Call the function and get the matched keywords
+    matched_keywords = predict_commit_outcome(commit_message)
+
+    if matched_keywords is not None:
+        print(f"Matched Keywords are: {matched_keywords}")
+
+    print("Prediction process completed.")
