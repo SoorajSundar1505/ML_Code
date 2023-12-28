b8123d88a2ec2bbb3b09d16496ffa4ba943677ff
Suraj S <130204034+SoorajSundar1505@users.noreply.github.com>
2023-12-28 15:44:02 +0530

    group test

--- getCommitMessage.py
+++ getCommitMessage.py
@@ -10,6 +10,7 @@ def predict_commit_outcome(commit_message, model_path='model.pkl', vectorizer_pa
     # Feature extraction
     new_commit_vectorized = vectorizer.transform([commit_message])
 
+    
     # Predict the outcome for the commit
     outcome_prediction = model.predict(new_commit_vectorized)
 
